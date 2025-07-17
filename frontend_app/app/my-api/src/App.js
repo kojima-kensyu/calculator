2d4a2a96d8bf7ccbbcf50fdecef5063b7712f5a8
mayu <mayu.k0219sun@gmail.com>
2025-07-17 23:08:44 +0900

    add counter

---
+++ frontend_app/app/my-api/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import  {MyApi} from './components/MyApi';
+
+function App() {
+  return (
+    <div className="App">
+      <MyApi />
+    </div>
+  );
+}
+
+export default App;
